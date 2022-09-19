c5d30ef4f52a6e628e8400447c878b4f0ce1d13f
Pablo Di Martino <pablo.dimartino@disney.com>
2022-09-19 18:49:53 -0300

    initial

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(req) {
+  const { nextUrl: url, geo } = req
+  
+  const country = geo.country || 'us'
+
+  let geoUrl = `/${country}${url.pathname}`
+  url.pathname = geoUrl
+    
+  return NextResponse.redirect(url)
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/about/:path*',
+}
---
+++ pages/us/about.tsx
@@ -0,0 +1,12 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      Hi from us
+    </div>
+  )
+}
